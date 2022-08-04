b7f5221a145381f8f058858eb6ba5405e1ad7a3b
Md. Belal Hossain <belal.cseai@gmail.com>
2022-08-04 13:32:31 +0600

    005 Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,23 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Belal");
+console.log(23);
+
+let firstName = "Belal";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let belal_hername = "BH";
+let $function = 27;
+
+let person = "belal";
+let PI = 3.1415;
+
+let myFirstJob = "Teacher";
+let myCurrentJob = "Programmer"
+
+let job1 = "teacher";
+let job2 = "programmer";
