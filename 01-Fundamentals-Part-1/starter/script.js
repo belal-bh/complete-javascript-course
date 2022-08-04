697ed006bd619d553dc0676d10b60ec541650298
Md. Belal Hossain <belal.cseai@gmail.com>
2022-08-04 14:40:58 +0600

    007 Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -10,6 +11,7 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+// Variable name conventions
 let belal_hername = "BH";
 let $function = 27;
 
@@ -21,3 +23,26 @@ let myCurrentJob = "Programmer"
 
 let job1 = "teacher";
 let job2 = "programmer";
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Belal');
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+// bug can occure
+console.log(typeof null);
